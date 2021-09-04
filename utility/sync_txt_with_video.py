9e4c3b45169b42a3c4ffd57d54b8b4b399162932
leanderFinck1999 <leander.finck@gmail.com>
2021-09-04 23:03:36 +0200

    synchronization works for real now.

--- utility/sync_txt_with_video.py
+++ utility/sync_txt_with_video.py
@@ -25,45 +25,24 @@ def get_number_of_words(preparation, nlp):
 class Sync:
     def __init__(self, recipe, nlp, history_of_tools_in_video):
         video_duration = len(history_of_tools_in_video)
-        word_count = get_number_of_words(recipe[db.RecipeWithVideoI.PREPARATION], nlp)
-
-        self.words_per_second = word_count / video_duration
+        self.word_count = get_number_of_words(recipe[db.RecipeWithVideoI.PREPARATION], nlp)
+        self.words_per_second = self.word_count / video_duration
         print("words_per_seconds: ", self.words_per_second)
 
         self.tools_in_video = history_of_tools_in_video
-        self.wait = False
-        self.word_remainder = 0
-        self.video_timestamp = 0
-        self.counter = 0
-        self.video_is_done = False
+        self.word_counter = 0
+        self.remainder = 0
+        self.list_index = 0
 
-    def update_index_values(self):
-        print(self.counter, int(self.words_per_second), self.counter > int(self.words_per_second))
-        print()
-        if self.counter > int(self.words_per_second):
-            self.word_remainder += (self.counter - self.words_per_second)
-            self.counter = 0
-            self.video_timestamp += self.word_remainder
-            print("incremented timestamp to: ", self.video_timestamp)
-        elif self.word_remainder >= 1:
-            self.word_remainder -= 1
-            self.video_timestamp += 1
-            print("incremented timestamp to: ", self.video_timestamp)
+    def get_cv_kitchenware(self):
+        if self.list_index < len(self.tools_in_video):
+            return self.tools_in_video[int(self.list_index)]
         else:
-            print("in else. no values changed")
+            return None
 
-    def get_cv_detected_kitchenware(self):
-        print("length of tools in counter: ", len(self.tools_in_video))
-        self.update_index_values()
+    def increment_index(self):
+        self.list_index += 1
 
-        video_detected_kitchenware = None
-        if round(self.video_timestamp) < len(self.tools_in_video):
-            video_detected_kitchenware = self.tools_in_video[round(self.video_timestamp)]
-        else:
-            self.video_is_done = True
+    def increase_counter(self, difference):
+        self.word_counter += difference
 
-        return video_detected_kitchenware
-
-    def increment_word_counter(self):
-        self.counter += 1
-        print("incremented word counter to: ", self.counter)
