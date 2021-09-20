c50195195388a252849dd9cc3cb479011d892fd4
leanderFinck1999 <leander.finck@gmail.com>
2021-09-20 22:04:33 +0200

    added evaluation for utensil_used_for. Truth values could be reviewed and both container and utensil used for need to be evaluated based on their antonyms. Also missing the other evaluations.

--- knowledge_base/eval_container_used_for.py
+++ knowledge_base/eval_container_used_for.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 import csv
 import ast
+from nltk.corpus import wordnet
+
+
+def get_synonyms(verbs):
+    synonyms = []
+    for verb in verbs:
+        synonyms.append(verb)
+        for syn in wordnet.synsets(verb):
+            for l in syn.lemmas():
+                synonym = l.name()
+                if synonym not in synonyms:
+                    synonyms.append(synonym)
+    print(len(synonyms))
+    return synonyms
 
 
 def get_true_verbs_for(container, ground_truths):
@@ -9,6 +23,35 @@ def get_true_verbs_for(container, ground_truths):
         if true_container_verbs[0] == container:
             return true_container_verbs[1].split(", ")
     return None
+
+
+def evaluate_container(predicted_container_verbs, true_verbs_for_container):
+    tmp = {'Precision': None, 'Recall': None, 'True Positive': {}, 'False Positive': {}, 'False Negative': []}
+    verbs_with_synonyms = get_synonyms(true_verbs_for_container)
+
+    for verb_key in predicted_container_verbs:
+        if verb_key in verbs_with_synonyms:
+            tmp['True Positive'][verb_key] = predicted_container_verbs[verb_key]
+        else:
+            tmp['False Positive'][verb_key] = predicted_container_verbs[verb_key]
+
+    for true_verbs in true_verbs_for_container:
+        if true_verbs not in predicted_container_verbs:
+            tmp['False Negative'].append(true_verbs)
+
+    tmp['Precision'] = (len(tmp['True Positive']) / (len(tmp['True Positive']) + len(tmp['False Positive'])))
+    tmp['Recall'] = (len(tmp['True Positive']) / (len(tmp['True Positive']) + len(tmp['False Negative'])))
+
+    return tmp
+
+
+def get_average(dic):
+    summation = 0
+    counter = 0
+    for verb_key in dic:
+        summation += dic[verb_key]
+        counter += 1
+    return summation / counter
 
 
 def main():
@@ -25,36 +68,22 @@ def main():
     for container_verbs in extracted_knowledge_list:
         curr_container = container_verbs[0]
         predicted_container_verbs = ast.literal_eval(container_verbs[1])
-        evaluation[curr_container] = {'Precision': None, 'Recall': None,
-                                      'True Positive': {},
-                                      'False Positive': {},
-                                      'False Negative': []}
 
         true_verbs_for_container = get_true_verbs_for(curr_container, ground_truth_list)
         assert true_verbs_for_container is not None, "container must be found"
 
-        for verb_key in predicted_container_verbs:
-            if verb_key in true_verbs_for_container:
-                evaluation[curr_container]['True Positive'][verb_key] = predicted_container_verbs[verb_key]
-            else:
-                evaluation[curr_container]['False Positive'][verb_key] = predicted_container_verbs[verb_key]
+        evaluation[curr_container] = evaluate_container(predicted_container_verbs, true_verbs_for_container)
 
-        for true_verbs in true_verbs_for_container:
-            if true_verbs not in predicted_container_verbs:
-                evaluation[curr_container]['False Negative'].append(true_verbs)
-
-        tp = len(evaluation[curr_container]['True Positive'])
-        fp = len(evaluation[curr_container]['False Positive'])
-        fn = len(evaluation[curr_container]['False Negative'])
         print("current container: ", curr_container)
         print("true verbs: ", true_verbs_for_container)
-        print("true positive: ", tp)
-        print("false positive: ", fp)
-        print("false positive list: ", list(evaluation[curr_container]['False Positive']))
-        print("false negative: ", fn)
-        print("false negative list: ", evaluation[curr_container]['False Negative'])
-        evaluation[curr_container]['Precision'] = (tp / (tp + fp))
-        evaluation[curr_container]['Recall'] = (tp / (tp + fn))
+        print("correct verbs (true positive): ", list(evaluation[curr_container]['True Positive']))
+        print("incorrect verbs (false positive): ", list(evaluation[curr_container]['False Positive']))
+        print("missing verbs (false negative): ", evaluation[curr_container]['False Negative'])
+        print("true positive: ", len(evaluation[curr_container]['True Positive']))
+        print("average occurrence of true detected verbs: ", get_average(evaluation[curr_container]['True Positive']))
+        print("false positive: ", len(evaluation[curr_container]['False Positive']))
+        print("average occurrence of falsely detected verbs: ", get_average(evaluation[curr_container]['False Positive']))
+        print("false negative: ", len(evaluation[curr_container]['False Negative']))
         print("precision: ", evaluation[curr_container]['Precision'])
         print("recall: ", evaluation[curr_container]['Recall'])
         print("\n\n")
---
+++ knowledge_base/eval_utensil_used_for.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import csv
+import ast
+from nltk.corpus import wordnet
+
+
+def get_synonyms(verbs):
+    synonyms = []
+    for verb in verbs:
+        synonyms.append(verb)
+        for syn in wordnet.synsets(verb):
+            for l in syn.lemmas():
+                synonym = l.name()
+                if synonym not in synonyms:
+                    synonyms.append(synonym)
+    print(len(synonyms))
+    return synonyms
+
+
+def get_true_verbs_for(utensil, ground_truths):
+    print(utensil)
+    for true_utensil_verbs in ground_truths:
+        if true_utensil_verbs[0] == utensil:
+            return true_utensil_verbs[1].split(", ")
+    return None
+
+
+def evaluate_utensil(predicted_utensil_verbs, true_verbs_for_utensil):
+    tmp = {'Precision': None, 'Recall': None, 'True Positive': {}, 'False Positive': {}, 'False Negative': []}
+    verbs_with_synonyms = get_synonyms(true_verbs_for_utensil)
+
+    for verb_key in predicted_utensil_verbs:
+        if verb_key in verbs_with_synonyms:
+            tmp['True Positive'][verb_key] = predicted_utensil_verbs[verb_key]
+        else:
+            tmp['False Positive'][verb_key] = predicted_utensil_verbs[verb_key]
+
+    for true_verbs in true_verbs_for_utensil:
+        if true_verbs not in predicted_utensil_verbs:
+            tmp['False Negative'].append(true_verbs)
+
+    tmp['Precision'] = (len(tmp['True Positive']) / (len(tmp['True Positive']) + len(tmp['False Positive'])))
+    tmp['Recall'] = (len(tmp['True Positive']) / (len(tmp['True Positive']) + len(tmp['False Negative'])))
+
+    return tmp
+
+
+def main():
+    extracted_knowledge = open('extracted_knowledge/utensils_used_for.csv')
+    extracted_knowledge_reader = csv.reader(extracted_knowledge)
+    next(extracted_knowledge_reader)
+    extracted_knowledge_list = list(extracted_knowledge_reader)
+
+    ground_truth = open('ground_truths/utensils_used_for_truth.csv')
+    ground_truth_list = list(csv.reader(ground_truth))
+
+    evaluation = {}
+
+    for predicted_utensil_with_verbs in extracted_knowledge_list:
+        curr_utensil = predicted_utensil_with_verbs[0]
+        predicted_utensil_verbs = ast.literal_eval(predicted_utensil_with_verbs[1])
+
+        true_verbs_for_utensil = get_true_verbs_for(curr_utensil, ground_truth_list)
+        if true_verbs_for_utensil is None:
+            continue
+
+        evaluation[curr_utensil] = evaluate_utensil(predicted_utensil_verbs, true_verbs_for_utensil)
+
+        print("current container: ", curr_utensil)
+        print("true verbs: ", true_verbs_for_utensil)
+        print("correct verbs (true positive): ", list(evaluation[curr_utensil]['True Positive']))
+        print("incorrect verbs (false positive): ", list(evaluation[curr_utensil]['False Positive']))
+        print("missing verbs (false negative): ", evaluation[curr_utensil]['False Negative'])
+        print("true positive: ", len(evaluation[curr_utensil]['True Positive']))
+        print("false positive: ", len(evaluation[curr_utensil]['False Positive']))
+        print("false negative: ", len(evaluation[curr_utensil]['False Negative']))
+        print("precision: ", evaluation[curr_utensil]['Precision'])
+        print("recall: ", evaluation[curr_utensil]['Recall'])
+        print("\n\n")
+
+
+if __name__ == '__main__':
+    main()
